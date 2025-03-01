803d806bd732c6e227d5954b719a4967bdd62eed
MhideTech <okefolahanolamide2006@gmail.com>
2025-03-01 23:49:48 +0100

    removed off color from ui

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-8 py-6 bg-purple-950 border-b-2 border-bg-secondary text-accent">
+    <nav className="flex justify-between items-center px-8 py-6 bg-primary border-b-2 border-bg-secondary text-accent">
       <div className="flex gap-4 items-center">
         <div className="border-r-2 pe-6">
           <h1 className="text-4xl">Logo</h1>
@@ -9,7 +9,7 @@ export default function Navbar() {
         <ul className="flex gap-4">
           <li>Home</li>
           <li>About</li>
-          <li>Books</li>
+          <li>Blog</li>
         </ul>
       </div>
       <div className="flex gap-4 z-10">
